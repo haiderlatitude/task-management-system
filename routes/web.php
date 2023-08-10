fa2a109aeaa1acae899f81a1c6cf03df353cf098
Haider Ali <wahabw259@gmail.com>
2023-08-10 15:27:03 +0500

    route file update

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/ex', function () {
-    $tasks = Task::all();
-    return view('admin.pages.reports.export', compact('tasks'));
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
