937cecae80287c7c65a9437b7c8c5cfc12f1a389
Haider Ali <wahabw259@gmail.com>
2023-08-10 16:13:21 +0500

    remove unused classes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\RoleNPermissionController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserReportController;
-use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 /*
